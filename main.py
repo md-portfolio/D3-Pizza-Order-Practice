59cef8d6f967350a572b15c1f6b3ba3de149b44d
md-portfolio <88002533+md-portfolio@users.noreply.github.com>
2022-09-27 19:40:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
-
 bill = 0
 
 if size == "S":
